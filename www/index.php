41b9ef8578feb82af97a908f794958584d3eb996
Alice Wonder <github@domblogger.net>
2018-04-30 14:52:32 -0700

    Add file doc

--- www/index.php
+++ www/index.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Redirect requests for index to blog.
+ *
+ * @package AWonderPHP\FlossWoff2
+ * @author  Alice Wonder <paypal@domblogger.net>
+ * @license https://opensource.org/licenses/MIT MIT
+ * @link    https://github.com/AliceWonderMiscreations/FlossWoff2
+ */
 header('Location: https://eroticaplexus.net/index.php/fonts-and-trackers/');
 exit;
 ?>
